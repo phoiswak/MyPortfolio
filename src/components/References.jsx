4a67c6ba92f81ba985266edacb19b8b36523e06c
PhoiswaK <phoiswak@gmail.com>
2026-03-04 19:40:15 +0200

    Adding new reference

--- src/components/References.jsx
+++ src/components/References.jsx
@@ -1,8 +1,24 @@
 export default function References() {
   return (
     <section id="references">
-      <p className="section-title"><span>05.</span> References <span></span></p>
+      <p className="section-title">
+        <span>05.</span> References <span></span>
+      </p>
       <p className="section-sub">People who can vouch for my work</p>
+
+      <div className="exp-card">
+        <div className="exp-header">
+          <div>
+            <div className="exp-title">Emmanuel Madzunye</div>
+            <div className="exp-company">redAcademy</div>
+          </div>
+          <div className="exp-date">Senior Software Dev. Lecturer</div>
+        </div>
+        <ul className="exp-list">
+          <li>📞 082 557 7449</li>
+          <li>✉️ manu.madzunye@gmail.com</li>
+        </ul>
+      </div>
 
       <div className="exp-card">
         <div className="exp-header">
@@ -18,5 +34,5 @@ export default function References() {
         </ul>
       </div>
     </section>
-  )
+  );
 }
